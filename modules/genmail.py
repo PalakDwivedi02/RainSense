f1e74b396cb1a23b4abcc27ab12b0e4e675bd83f
Kinshuk Goel <103813028+RpM-Kinshuk@users.noreply.github.com>
2023-03-08 02:22:26 +0530

    Added Logo to the mail content

--- modules/genmail.py
+++ modules/genmail.py
@@ -1,44 +1,46 @@
 def generate_mail(rainfall, month, day, temp, sphum, relhum):
     mail_content = f"""\
             <html>
-                <head>
-                    <div style="font-weight: 1000"><u>Rainfall Prediction Result</u></div>
-                </head>
+            <head>
+                <div style="font-weight: 1000; color: white;"><u>Rainfall Prediction Result</u></div>
+            </head>
 
-                <body>
-                    <p style="color: green;">
-                        <b>
-                            Hello, this is an automated message from the Rainfall Prediction Page!
-                        </b>
-                    </p>
-                    <p>
-                    <div style="color: black">
-                        According to the entered data: <br>
+            <body style="background-color: black;">
+                <br> <img src="https://i.imgur.com/PiZMOCp.png" width="34%" height="51%"
+                    style="vertical-align:middle; align-items: center;">
+                <p style="color: green;">
+                    <b>
+                        Hello, this is an automated message from the Rainfall Prediction Page!
+                    </b>
+                </p>
+                <p>
+                <div style="color: white">
+                    According to the entered data: <br>
+                </div>
+                <div style="color: goldenrod; font-weight: 520;">
+                    The Date is: {month}, {day}. <br>
+                    The Temperature is: {temp} degrees Celsius. <br>
+                    The Specific Humidity is: {sphum} g/kg. <br>
+                    The Relative Humidity is: {relhum} %. <br>
+                </div>
+                </p>
+                <p style="color: red; font-weight: 750;">
+                    The Predicted Rainfall is: {rainfall:.2f} mm.
+                </p>
+                <p style="color: white;">
+                    (Feel free to leave us feedback on our <a
+                        href="https://github.com/PalakDwivedi02/Rainfall-Prediction-using-ML">Github Page</a>!)
+                </p>
+                <p>
+                <div style="color: green;">
+                    Thank you for using our service!
+                    <div style="color: royalblue; font-weight: 750;"> <br>
+                        Regards,<br>
+                        <a href="https://www.linkedin.com/in/kinshuk-goel/">Kin</a> and <a
+                            href="https://www.linkedin.com/in/palak-dwivedi-017437247/">Bells</a> :3
                     </div>
-                    <div style="color: goldenrod; font-weight: 520;">
-                        The Date is: {month}, {day}. <br>
-                        The Temperature is: {temp} degrees Celsius. <br>
-                        The Specific Humidity is: {sphum} g/kg. <br>
-                        The Relative Humidity is: {relhum} %. <br>
-                    </div>
                     </p>
-                    <p style="color: red; font-weight: 750;">
-                        The Predicted Rainfall is: {rainfall:.2f} mm.
-                    </p>
-                    <p style="color: black;">
-                        (Feel free to leave us feedback on our <a
-                            href="https://github.com/PalakDwivedi02/Rainfall-Prediction-using-ML">Github Page</a>!)
-                    </p>
-                    <p>
-                    <div style="color: green;">
-                        Thank you for using our service!
-                        <div style="color: royalblue; font-weight: 750;"> <br>
-                            Regards,<br>
-                            <a href="https://www.linkedin.com/in/kinshuk-goel/">Kin</a> and <a
-                                href="https://www.linkedin.com/in/palak-dwivedi-017437247/">Bells</a> :3
-                        </div>
-                        </p>
-                </body>
+            </body>
             </html>
         """#.format(month, day, temp, sphum, relhum, rainfall)
     return mail_content
